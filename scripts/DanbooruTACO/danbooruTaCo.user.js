1c96dcc706b203556e2e243df2e70118c4e407ab
Ioann <vanja-san@users.noreply.github.com>
2025-08-26 20:36:52 +0700

    Update danbooruTaCo.user.js

--- scripts/DanbooruTACO/danbooruTaCo.user.js
+++ scripts/DanbooruTACO/danbooruTaCo.user.js
@@ -8,7 +8,7 @@
 // @author          vanja-san
 // @license         MIT
 // @match           https://danbooru.donmai.us/*
-// @icon            https://danbooru.donmai.us/favicon.ico
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=donmai.us
 // @downloadURL     https://raw.githubusercontent.com/vanja-san/JS-UserScripts/main/scripts/DanbooruTACO/danbooruTaCo.user.js
 // @updateURL       https://raw.githubusercontent.com/vanja-san/JS-UserScripts/main/scripts/DanbooruTACO/danbooruTaCo.user.js
 // @grant           GM_addStyle
