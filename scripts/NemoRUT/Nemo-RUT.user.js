b7c2f5a665164f9af59497d0d31a7c4577eab8fb
Ioann <vanja-san@users.noreply.github.com>
2025-08-26 20:37:32 +0700

    Update Nemo-RUT.user.js

--- scripts/NemoRUT/Nemo-RUT.user.js
+++ scripts/NemoRUT/Nemo-RUT.user.js
@@ -7,6 +7,7 @@
 // @version      1.0.3
 // @author       vanja-san
 // @match        https://*.nexusmods.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nexusmods.com
 // @downloadURL  https://raw.githubusercontent.com/vanja-san/JS-UserScripts/main/scripts/NemoRUT/Nemo-RUT.user.js
 // @updateURL    https://raw.githubusercontent.com/vanja-san/JS-UserScripts/main/scripts/NemoRUT/Nemo-RUT.user.js
 // @grant        none
