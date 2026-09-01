bd454ea8515ed17c160517c3815b3ce317ef9e20
vanja-san <vanja-san@users.noreply.github.com>
2026-09-01 18:33:47 +0000

    deploy: f254e91b8e4658b24ae7da7b0de1727b7b4ed9f0

--- scripts/Nexus Russian Localizer/translations.js
+++ scripts/Nexus Russian Localizer/translations.js
@@ -1417,7 +1417,7 @@ const DICTIONARIES = {
       "p > a": "модов",
       "button > span": "Моды",
       "li > p": "Моды",
-      "#more-mods-header": "Моды"
+      "h2#more-mods-header": "Моды"
     },
     "Members": {
       "p > span": "Участник",
